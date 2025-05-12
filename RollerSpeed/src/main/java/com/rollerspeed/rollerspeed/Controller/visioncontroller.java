6202a7b81c180541ccdbb478455de0ce9a574382
Gercod <gerobc97@gmail.com>
2025-05-11 20:06:12 -0500

    pruebas de servicios de paginas

--- RollerSpeed/src/main/java/com/rollerspeed/rollerspeed/Controller/visioncontroller.java
+++ RollerSpeed/src/main/java/com/rollerspeed/rollerspeed/Controller/visioncontroller.java
@@ -1,22 +1,19 @@
 package com.rollerspeed.rollerspeed.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 @RequestMapping("/")
 
-public class indexcontroller {
-    @GetMapping("index")
-    public String index() {
-        return "index";
+
+public class visioncontroller {
+
+        @GetMapping("vision")
+    public String vision() {
+        return "vision";
 
     }
-
-
     
-
 }
